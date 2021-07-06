057e580aba94f46fdac7ef1ad70e452bc108735b
Manolo Enriquez <noloenriquez@gmail.com>
2021-07-06 15:11:24 +0800

    github pages deploy

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import '@fortawesome/fontawesome-free/css/all.css'
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
