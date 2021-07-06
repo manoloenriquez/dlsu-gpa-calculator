4f47a8699bc88db94c909c7e3844e413cfe8dbf5
Manolo Enriquez <noloenriquez@gmail.com>
2021-07-06 15:03:19 +0800

    v1

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
--- script.js
+++
@@ -1,3 +0,0 @@
-function calculate() {
-  console.log('hello world')
-}
